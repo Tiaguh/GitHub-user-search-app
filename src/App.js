079d4057783aa1b24f033b46c359bd13b82c7672
Tiaguh <tiagosingup@gmail.com>
2024-05-16 11:20:52 -0300

    styling more containers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="container">
 
       <header>
-        <h1>devfinder</h1>
+        <h1>DevFinder</h1>
         <button>Light</button>
       </header>
 
@@ -16,48 +16,56 @@ export default function App() {
         <button>Search</button>
       </div>
 
-      <main>
-        <img alt="User-img" />
+      <div className="user-info-container" >
+
+        <main>
+
+          <img alt="user-img" />
+
+          <div className="user-info">
+
+            <div>
+              <h1>UserName</h1>
+              <h2>NickName</h2>
+              <h3>Bio</h3>
+            </div>
+
+            <div>
+              <p>Joined</p>
+            </div>
+
+          </div>
+
+        </main>
+
+        <section>
+
+          <div>
+            <h1>Repos</h1>
+            <p>Repos Number</p>
+          </div>
+
+          <div>
+            <h1>Followers</h1>
+            <p>Followers Number</p>
+          </div>
+
+          <div>
+            <h1>Following</h1>
+            <p>Following Number</p>
+          </div>
+
+        </section>
 
         <div>
-          <h1>UserName</h1>
-          <h2>NickName</h2>
-          <h3>Bio</h3>
+          <h3>Location</h3>
+          <h3>Twitter</h3>
+          <h3>Site</h3>
+          <h3>Company</h3>
         </div>
 
-        <div>
-          <p>Joined</p>
-        </div>
 
-      </main>
-
-      <section>
-
-        <div>
-          <h1>Repos</h1>
-          <p>Repos Number</p>
-        </div>
-
-        <div>
-          <h1>Followers</h1>
-          <p>Followers Number</p>
-        </div>
-
-        <div>
-          <h1>Following</h1>
-          <p>Following Number</p>
-        </div>
-
-      </section>
-
-      <div>
-        <h3>Location</h3>
-        <h3>Twitter</h3>
-        <h3>Site</h3>
-        <h3>Company</h3>
       </div>
-
-
     </div>
   );
 }
